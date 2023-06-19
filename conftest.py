235a12a2106ed456a0f862754ba48d105e2be4c4
Aleksandr Tekutev <tekutiev.sasha@gmail.com>
2023-06-19 14:24:53 +0100

    Added 2 test_routes tests

--- conftest.py
+++ conftest.py
@@ -1,17 +1,12 @@
 import pytest
 
-
-@pytest.fixture
-def author(django_user_model):
-    return django_user_model.objects.create(username='Autor')
+from news.models import News
 
 
 @pytest.fixture
-def author_client(author, client):
-    client.force_login(author)
-    return client
-
-
-def test_with_client(client):
-    response = client.get('/')
-    assert response.status_code == 200 
+def news():
+    news = News.objects.create(
+        title='Title',
+        text='Text',
+    )
+    return news
--- news/pytest_tests/test_routes.py
+++ news/pytest_tests/test_routes.py
@@ -3,14 +3,23 @@ from http import HTTPStatus
 
 from django.urls import reverse
 
+pytestmark = pytest.mark.django_db
+
 
 @pytest.mark.parametrize(
     'name',
     ('news:home', 'users:login', 'users:logout', 'users:signup')
 )
 def test_pages_availability_for_anonymous_user(client, name):
-    '''Главная страница, страницы регистрации пользователей,
+    '''Главная страница, страницы отдельной новости, регистрации пользователей,
     входа в учётную запись и выхода из неё доступны анонимным пользователям.'''
     url = reverse(name)
+    response = client.get(url)
+    assert response.status_code == HTTPStatus.OK
+
+
+def test_details_availability_for_anonymous_user(client, news):
+    '''Страница отдельной новости доступна анонимному пользователю'''
+    url = reverse('news:detail', args=(news.pk,))
     response = client.get(url)
     assert response.status_code == HTTPStatus.OK
